784428c242da7593110f563ea8aeeec8d7725607
Refi Go <64242999+refigo@users.noreply.github.com>
2022-11-16 13:53:52 +0900

    feat: add operator module - #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AttendanceModule } from './attendance/attendance.module';
+import { OperatorModule } from './operator/operator.module';
 
 @Module({
-  imports: [UserModule, AttendanceModule],
+  imports: [UserModule, AttendanceModule, OperatorModule],
   controllers: [AppController],
   providers: [AppService],
 })
