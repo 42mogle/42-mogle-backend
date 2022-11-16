28d32befacd31de99a723e07709c86b93c7ef0c1
Refi Go <64242999+refigo@users.noreply.github.com>
2022-11-16 13:51:53 +0900

    feat: add attendance module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AttendanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
