e5b587c946715c1155d28423389944f195964d2e
Refi Go <64242999+refigo@users.noreply.github.com>
2022-11-16 13:55:36 +0900

    feat: add statistic module - #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AttendanceModule } from './attendance/attendance.module';
 import { OperatorModule } from './operator/operator.module';
+import { StatisticModule } from './statistic/statistic.module';
 
 @Module({
-  imports: [UserModule, AttendanceModule, OperatorModule],
+  imports: [UserModule, AttendanceModule, OperatorModule, StatisticModule],
   controllers: [AppController],
   providers: [AppService],
 })
