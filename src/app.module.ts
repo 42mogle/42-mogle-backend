3571d99ab232c0d3088f46f057059ea41e48a0d6
Refi Go <64242999+refigo@users.noreply.github.com>
2022-11-27 16:33:36 +0900

    fix: Remove importing User entitiy

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,8 @@ import { DayInfo } from './dbmanager/entities/day_info.entity';
 import { MonthInfo } from './dbmanager/entities/month_info.entity';
 import { MonthlyUsers } from './dbmanager/entities/monthly_users.entity';
 import { DbmanagerService } from './dbmanager/dbmanager.service';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { Auth } from './auth/entities/auth.entity';
 // import { HttpModule } from '@nestjs/axios';
