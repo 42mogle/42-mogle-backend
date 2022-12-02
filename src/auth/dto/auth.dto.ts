00b1c468cfcd2654574e22aac032c802d660c673
refigo <has0521@daum.net>
2022-12-02 14:06:04 +0900

    feat: Set AuthDto Swagger ApiProperty

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class AuthDto {
+    @ApiProperty()
     intraId : string;
+
+    @ApiProperty()
     password : string;
+
+    @ApiProperty()
     photoUrl : string;
+
+    @ApiProperty()
     isOperator : boolean;
 }
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,7 +4,7 @@ export const typeORMConfig : TypeOrmModuleOptions = {
 	type: 'postgres',
 	host: 'localhost',
 	port: 5432,
-	username: 'postgres',
+	username: 'mgo',	// info: 클러스터 맥에서 homebrew로 설치한 DB 사용시 등록된 user명으로 사용 (ex. mgo)
 	password: 'postgres',
 	database: 'test',
 	entities: [
