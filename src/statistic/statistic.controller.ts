7a3cbcd290a9c6fc47da5fc8dc00718eda1b47fd
Refi Go <64242999+refigo@users.noreply.github.com>
2022-12-04 17:57:03 +0900

    feat: Document GET /statistic/{intraId}/userAttendanceList - #45

--- src/statistic/statistic.controller.ts
+++ src/statistic/statistic.controller.ts
@@ -2,17 +2,39 @@ import { ConsoleLogger, Controller, Get, Param, UseGuards } from '@nestjs/common
 import { Attendance } from 'src/dbmanager/entities/attendance.entity';
 import { StatisticService } from './statistic.service';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Statistic')
 @Controller('statistic')
 export class StatisticController {
 	constructor(private readonly statisticService: StatisticService) {}
 
-	@UseGuards(JwtAuthGuard)
 	@Get("/:intraId/userAttendanceList")
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth('access-token')
+	@ApiOperation({
+		summary: 'get the user attendance list',
+		description: '유저 출석 정보 리스트 요청'
+	})
+	@ApiParam({
+		name: 'intraId',
+		type: String,
+	})
+	@ApiResponse({
+		status: 200, 
+		description: 'Success', 
+		// todo: type: Attendance list
+	})
+	@ApiResponse({
+		status: 401,
+		description: 'Error: Unauthorized (Blocked by JwtAuthGuard: No JWT access-token)'
+	})
+	@ApiResponse({
+		status: 403,
+		description: 'Forbidden'
+	})
 	async getUserAttendanceList(@Param("intraId") intraId: string): Promise<Attendance[]> {
-		console.log("왔니?");
+		console.log("[GET /statistic/{intraId}/userAttendanceList] requested.");
 		return await this.statisticService.getAttendanceList(intraId);
 	}
 
