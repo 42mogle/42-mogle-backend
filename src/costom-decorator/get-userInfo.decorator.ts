74466d0b4732eb5b8f554d794c4c557bef2b317a
Refi Go <64242999+refigo@users.noreply.github.com>
2022-12-07 18:55:21 +0900

    test: Add console.log to check request.user for testing

--- src/costom-decorator/get-userInfo.decorator.ts
+++ src/costom-decorator/get-userInfo.decorator.ts
@@ -5,8 +5,12 @@ export const GetUserInfo = createParamDecorator((data, ctx: ExecutionContext): U
 	const req = ctx.switchToHttp().getRequest();
 	const retUserInfo = req.userInfo;
 	console.log(`[ In GetUserInfo ]`);
-	console.log(`request:`);
-	console.log(req);
+	//console.log(`request:`);
+	//console.log(req);
+	console.log(`request.user:`);
+	console.log(req.user);
+	console.log(`request.user.UserInfo:`);
+	console.log(req.user.UserInfo);
 	console.log(`retUserInfo:`);
 	console.log(retUserInfo);
 	return req.userInfo;
