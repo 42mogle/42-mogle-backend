ceabefe535ff76622a32f08dbd194ce86eeeb5b6
Refi Go <64242999+refigo@users.noreply.github.com>
2022-12-07 22:51:29 +0900

    feat: Set text in typeorm.cofig

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,14 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
+// todo: set all in .env
 export const typeORMConfig : TypeOrmModuleOptions = {
 	type: 'postgres',
 	host: 'localhost',
 	port: 5432,
-	username: 'postgres',	// info: 클러스터 맥에서 homebrew로 설치한 DB 사용시 등록된 user명으로 사용 (ex. mgo)
+	username: 'postgres',
 	password: 'postgres',
 	database: 'beta_test2',
 	entities: [
 		__dirname + '/../**/*.entity.{js,ts}'
 	],
-	synchronize: true		// deploy할 때 false로 해야 DB table columns가 안전함
+	synchronize: true
 }
