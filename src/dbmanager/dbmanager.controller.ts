3d0f39fdd75e5d0269fb7c5180e5bcbe6b97b506
Refi Go <64242999+refigo@users.noreply.github.com>
2022-12-07 16:18:13 +0900

    feat: Set dbmanager swagger API for JWT

--- src/dbmanager/dbmanager.controller.ts
+++ src/dbmanager/dbmanager.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, UnauthorizedException } from '@nestjs/common';
 import { DbmanagerService } from './dbmanager.service';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserInfo } from './entities/user_info.entity';
 import { GetUserInfo } from 'src/costom-decorator/get-userInfo.decorator';
 
@@ -10,14 +10,36 @@ import { GetUserInfo } from 'src/costom-decorator/get-userInfo.decorator';
 export class DbmanagerController {
 	constructor(private readonly dbmanagerService: DbmanagerService) { }
 
-	@UseGuards(JwtAuthGuard)
 	@Post('/set/totalMonthInfo') // 해달 달의 정보와 그달의 모든 일자에 대한 정보를 데이터로 남겨논다 //크론
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth('access-token')
+	@ApiOperation({summary: 'set total month info'})
+	@ApiResponse({
+		status: 201, 
+		description: 'Success', 
+		// todo: Set type using dto
+	})
+	@ApiResponse({
+		status: 401,
+		description: 'Error: Unauthorized (Blocked by JwtAuthGuard)'
+	})
 	setTotalMonthInfo(@GetUserInfo() userInfo: UserInfo) {
 		return this.dbmanagerService.setTotalMonthInfo(userInfo);
 	}
-
-	@UseGuards(JwtAuthGuard)
+	
 	@Post("/setcurrent")
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth('access-token')
+	@ApiOperation({summary: 'set current'})
+	@ApiResponse({
+		status: 201, 
+		description: 'Success', 
+		// todo: Set type using dto
+	})
+	@ApiResponse({
+		status: 401,
+		description: 'Error: Unauthorized (Blocked by JwtAuthGuard)'
+	})
 	testSetCurrent(@GetUserInfo() userInfo: UserInfo) {
 		if (userInfo.isOperator === false)
 			throw new UnauthorizedException("Not Operator");
