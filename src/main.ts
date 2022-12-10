d7ec17ef1a1069725f1c43f334cc8c29ba524102
Refi Go <64242999+refigo@users.noreply.github.com>
2022-12-10 16:51:03 +0900

    fix: Modify typeORMConfig for using database configuration - #64

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -35,5 +36,6 @@ async function bootstrap() {
   });
 
   await app.listen(port);
+  Logger.log(`42mogle app running on port ${port}`);
 }
 bootstrap();
