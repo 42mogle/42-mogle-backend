97e899f8003af681d46b5cc0e546d4825d5a338c
minsubro <mmss3265@gmail.com>
2023-01-03 09:57:41 +0900

    feat:currentcountLogTest

--- src/dbmanager/dbmanager.service.ts
+++ src/dbmanager/dbmanager.service.ts
@@ -406,6 +406,7 @@ export class DbmanagerService {
 
 	async updateThisMonthCurrentCount() {
 		const monthInfo: MonthInfo = await this.getThisMonthInfo();
+		this.logger.log("currentAttendance = " + monthInfo.currentAttendance)
 		this.monthInfoRepository.update(monthInfo.id, {
 			currentAttendance: monthInfo.currentAttendance + 1
 		})
