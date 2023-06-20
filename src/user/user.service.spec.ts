2eea159a28bb8ed770003125174b6f57c52fa154
refigo <has0521@daum.net>
2023-06-20 17:36:10 +0900

    TEST: Set DbmanagerService mocking for testing UserService

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DbmanagerService } from '../dbmanager/dbmanager.service';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -6,7 +7,17 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: DbmanagerService,
+          useValue: {
+            // Mock the methods of DbmanagerService that are used by UserService here.
+            // For example, if UserService uses a method 'getUser', you could do:
+            // getUser: jest.fn().mockImplementation(() => /* return value here */),
+          }
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
